c143c0e239d4f71d8724e699e886fa2afd4a9827
chenping <chenping@wecash.net>
2020-03-03 11:29:09 +0800

    header style coding

--- src/module/Home/index.jsx
+++ src/module/Home/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import {renderRoutes} from "react-router-config";
+import {Top, Tab, TabItem} from "./style";
+import {NavLink} from "react-router-dom";
 
 function Home(props) {
   const {route} = props;
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont">&#xe676;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont">&#xe507;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected"><TabItem><span> 推荐 </span></TabItem></NavLink>
+        <NavLink to="/singers" activeClassName="selected"><TabItem><span> 歌手 </span></TabItem></NavLink>
+        <NavLink to="/rank" activeClassName="selected"><TabItem><span> 排行版 </span></TabItem></NavLink>
+      </Tab>
+      {/*<div>Home</div>*/}
       {/*renderRoutes只能渲染一层路由，需要在home中再调用一次*/}
       {renderRoutes(route.routes)}
     </div>
